1a9d3f63c16e7e6f63c77474758f5ebbba11e267
Jonathan Tirapegui <damiantirapegui@gmail.com>
2024-04-24 15:07:43 +0200

    Testar med andreas

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./components/Buttons/style.css";
+import "./index.css";
+import { Home } from "./pages/home/Home";
+import { AddCard } from "./pages/addcard/AddCard";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/AddCard" element={<AddCard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
