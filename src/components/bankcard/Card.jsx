c5c97d0486cf4fb2c96f48bbbc74b85b30a1ca9e
Jonathan Tirapegui <damiantirapegui@gmail.com>
2024-04-29 18:14:26 +0200

    Interaktiv CardForm.jsx

--- src/components/bankcard/Card.jsx
+++ src/components/bankcard/Card.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
-export default function Card() {
+export default function Card({
+  cardNumber,
+  cardholderName,
+  validThru,
+  vendor,
+}) {
+  let vendorIconSrc = "";
+
+  switch (vendor) {
+    case "1":
+      vendorIconSrc = "./src/assets/bitcoin.png";
+      break;
+    case "2":
+      vendorIconSrc = "./src/assets/ninja.png";
+      break;
+    case "3":
+      vendorIconSrc = "./src/assets/chain.png";
+      break;
+    case "4":
+      vendorIconSrc = "./src/assets/evil.png";
+      break;
+    default:
+      vendorIconSrc = "";
+  }
+
   return (
     <article className="card">
       <header className="card-header">
@@ -14,27 +38,22 @@ export default function Card() {
             src="./src/assets/chip-dark.png"
             className="chip"
             alt="chip-icon"
-            
           />
         </div>
         <div className="bank-logo-container">
-          <img
-            src="./src/assets/bitcoin.png"
-            className="bank-logo"
-            alt="bank-logo"
-          />
+          <img src={vendorIconSrc} className="bank-logo" alt="bank-logo" />
           {/* <img src={`/${vendor}-logo.png`} alt={`${vendor} Logo`} /> */}
         </div>
       </header>
       <section className="card-body">
-        <div className="card-number">XXXX XXXX XXXX XXXX</div>
+        <div className="card-number">{cardNumber}</div>
         <div className="card-labels">
           <p className="card-label">Cardholder name</p>
           <p className="card-label">Valid thru</p>
         </div>
         <div className="card-content">
-          <p className="card-holder">Firstname Lastname</p>
-          <p className="valid-thru"> MM / YY </p>
+          <p className="card-holder">{cardholderName}</p>
+          <p className="valid-thru"> {validThru} </p>
         </div>
       </section>
     </article>
